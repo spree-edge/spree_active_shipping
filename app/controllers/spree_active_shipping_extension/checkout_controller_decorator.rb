55280703e1bdd52f38f3336c061061efeb9cf4b6
Vishal <vishal@bluebash.co>
2022-07-26 18:31:06 +0530

    Refactored the code to make it work for spree 4.4

--- app/controllers/spree_active_shipping_extension/checkout_controller_decorator.rb
+++ app/controllers/spree_active_shipping_extension/checkout_controller_decorator.rb
@@ -1,15 +1,15 @@
-# handle shipping errors gracefully during checkout
-module SpreeActiveShippingExtension::CheckoutControllerDecorator
-  def self.prepended(base)
-    base.rescue_from Spree::ShippingError, with: :handle_shipping_error
-  end
+# # handle shipping errors gracefully during checkout
+# module SpreeActiveShippingExtension::CheckoutControllerDecorator
+#   def self.prepended(base)
+#     base.rescue_from Spree::ShippingError, with: :handle_shipping_error
+#   end
 
-  private
+#   private
 
-  def handle_shipping_error(e)
-    flash[:error] = e.message
-    redirect_to checkout_state_path(:address)
-  end
-end
+#   def handle_shipping_error(e)
+#     flash[:error] = e.message
+#     redirect_to checkout_state_path(:address)
+#   end
+# end
 
-::Spree::CheckoutController.prepend(SpreeActiveShippingExtension::CheckoutControllerDecorator)
+# ::Spree::CheckoutController.prepend(SpreeActiveShippingExtension::CheckoutControllerDecorator)
